01345ad7edc0059773b881a5d4d52b5a8d63b9a3
dipeshsubedi11 <dsubedi805@gmail.com>
2024-08-14 19:10:53 +0545

    first commit

---
+++ oop.py
@@ -0,0 +1,16 @@
+class animal:
+    def  __init__(self, name):
+    self .name = name
+    def eat(self):
+            print("i can eat")
+     def sleep(self):
+                print("i can sleep")
+
+class dog(animal):
+    def bark(self):
+        print("i can bark")  
+
+dog1 = dog()
+dog.bark("hauhau")
+dog.sleep
+
