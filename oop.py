edbd051c809e420037d9ff5963f8173989fe353a
dipeshsubedi77 <dipeshfitness74@gmail.com>
2024-08-14 20:46:54 +0545

    dipesh

--- oop.py
+++ oop.py
@@ -1,16 +1,55 @@
-class animal:
-    def  __init__(self, name):
-    self .name = name
-    def eat(self):
-            print("i can eat")
-     def sleep(self):
-                print("i can sleep")
+# # to create a class
+# class employee:
+    # x = int(5)
 
-class dog(animal):
-    def bark(self):
-        print("i can bark")  
 
-dog1 = dog()
-dog.bark("hauhau")
-dog.
+    # print(employee)    
+
+
+# to create an obj
+# class myclass:
+#     x = 5
+    
+# p1 = myclass()
+# print(p1.x)
+
+#  class attributes
+# class cars:
+#     color = "red"
+#     brand = "tesla"
+# car1 = cars()
+# print(cars.color)
+# print(cars.brand)
+
+   # constructor
+
+# class Student:
+#     def __init__(self, fullname):
+#         self.name = fullname
+#         print("adding new students")
+
+
+# s1 = Student("dipesh")
+# print(s1.name)
+# class emplyee:
+#     def __init__(self,fullname,roll):
+#         self.name = fullname
+#         self.roll = 19
+# s1 =  emplyee("hellobro",12)
+
+
+# print(s1.name,s1.roll)
+
+
+
+# class student:
+#     name = "anynomous"
+
+#     def __init__(self,name,address,roll):
+#         self.name = name
+#         self.roll = roll
+#         self.address = address
+
+# s1 = student("dopes","rmbzr",12)
+# print(s1.name,s1.roll,s1.address)
 
